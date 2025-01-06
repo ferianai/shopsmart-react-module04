eee2ba16e1e4b049eac56fc418e0675305d208b4
ferianai <ferianaisti@gmail.com>
2025-01-06 17:58:44 +0700

    add file

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Home: React.FC = () => {
   return (
-    <div className="flex justify-center items-center h-screen bg-gray-100">
+    <div className="flex justify-center h-screen">
       <h1 className="text-3xl font-bold">Welcome to ShopSmart!</h1>
     </div>
   );
